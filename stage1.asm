e73814f919f27bbaf38d658b4eaab09631c00791
revenn <m.okroy@hotmail.com>
2016-06-15 10:56:07 +0200

    Added Bochs config file

--- stage1.asm
+++ stage1.asm
@@ -1,3 +1,12 @@
-[16 bits]
+[bits 16]
+; kiedy BIOS zaczyna szukac bootloadera
+; to szuka go pod specyficznym ustandaryzowanym adresem
+; 0x7c00
+; Adresy w 16bit trybie x86 wyraza sie jako
+; SEGMENT:OFFSET
+; adres = segment * 0x10 + offset
+; SEGMENT znajduje sie w rejestrze segmentowym CS
+; dalej mamy rejestr IP ktory wskazuje na nast. instrukcje
 
-nop
+jmp $	;EB FE skok w to samo miejsce, petla nieskonczona
+
